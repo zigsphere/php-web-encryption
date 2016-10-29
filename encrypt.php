b4d5ea3eb169af5e8a21297f2ef4dea1607d4d67
Joseph Ziegler <zieglerj@zigsphere.com>
2016-10-28 21:30:40 -0700

    Added php and web files

---
+++ encrypt.php
@@ -0,0 +1,26 @@
+<?php
+
+if(isset($_POST['password'])) $password = $_POST['password'];
+
+?>
+<!DOCTYPE>
+<html>
+<head>
+<title>Password Encryption</title>
+<link href="/style/style.css" rel="stylesheet" type="text/css"/>
+
+</head>
+<body>
+  <div id="content">
+
+    <p>
+      <?php
+	     if(isset($password)) foreach (hash_algos() as $v) {
+        $r = hash($v, $password, false);
+		    if(isset($password)) printf("<b><u>%-12s:</u></b> %s<br />", $v, $r);
+      }
+      ?>
+    </p>
+  </div>
+</body>
+</html>
